3ae02850fd4359c4bd018b4a2337c7c42a78f8e1
mfasDa <markus.fasel@web.de>
2014-03-21 16:47:03 +0100

    Initial commit

---
+++ src/idico/checkin/core/IndicoCheckinApp.java
@@ -0,0 +1,12 @@
+package idico.checkin.core;
+
+import idico.checkin.core.gui.IndicoCheckinAppMainGui;
+
+public class IndicoCheckinApp {
+
+	public static void main(String[] args) {
+		// Run the gui
+		IndicoCheckinAppMainGui mygui = new IndicoCheckinAppMainGui();
+	}
+
+}
