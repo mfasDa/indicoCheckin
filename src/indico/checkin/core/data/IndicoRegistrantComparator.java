59e1b87fb72a660db3d3f5d1e5b0419d1d5a0d57
Steffen Weber <s.weber@gsi.de>
2014-05-16 17:01:14 +0200

    compare firstname if lastname identical

--- src/indico/checkin/core/data/IndicoRegistrantComparator.java
+++ src/indico/checkin/core/data/IndicoRegistrantComparator.java
@@ -30,6 +30,12 @@ public class IndicoRegistrantComparator  implements Comparator<IndicoRegistrant>
 
 	@Override
 	public int compare(IndicoRegistrant o1, IndicoRegistrant o2) {
-		return o1.getSurname().toLowerCase().compareTo(o2.getSurname().toLowerCase());
+		if( o1.getSurname().toLowerCase().compareTo(o2.getSurname().toLowerCase()) == 0){
+			return o1.getFirstName().toLowerCase().compareTo(o2.getFirstName().toLowerCase());
+			
+		}
+		else{
+			return o1.getSurname().toLowerCase().compareTo(o2.getSurname().toLowerCase());
+		}
 	}
 }
